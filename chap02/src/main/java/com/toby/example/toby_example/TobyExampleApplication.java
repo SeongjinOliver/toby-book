709bb04fec50fd2aa7a83529b1e1e9cff450dd88
Seongjin Yoo <yoosngj@gmail.com>
2020-07-26 20:32:18 +0900

    Start chap02

---
+++ chap02/src/main/java/com/toby/example/toby_example/TobyExampleApplication.java
@@ -0,0 +1,13 @@
+package com.toby.example.toby_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TobyExampleApplication.class, args);
+  }
+
+}
